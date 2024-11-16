444c7a41c75dcafd5a520ad57645101ee9e60bcf
Omar Khaled <34315215+Omarkh98@users.noreply.github.com>
2024-11-16 22:13:33 +0100

    EDA, Problem Statement, and Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,12 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_details(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
 
     file_name = exc_tb.tb_frame.f_code.co_filename
 
-    error_message = "Error Occured In Python Script Name [{0}], Line # [{1}], Error Msg [{2}]".format(
+    error_message = "Error Occured In Python Script Name: [{0}], Line # [{1}], Error Msg [{2}]".format(
         file_name, exc_tb.tb_lineno, str(error)
     )
 
